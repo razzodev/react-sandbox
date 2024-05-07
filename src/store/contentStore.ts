cca2c3f3c42de285e961529897a885e38d2d4422
razzodev <razzo.des@gmail.com>
2024-05-07 18:08:40 +0530

    add(state): userData --initial

--- src/store/contentStore.ts
+++ src/store/contentStore.ts
@@ -5,6 +5,7 @@ type State = {
   language: string;
   wordList: any[];
   currPage: string;
+  userData: any;
 };
 
 type Actions = {
@@ -12,10 +13,12 @@ type Actions = {
   addWord: (data: any) => void;
   removeWord: (payload: any) => void;
   setCurrPage: (page: string) => void;
+  setUserData: (data: any) => void;
 };
 
-export const useAuthStore = create<State & Actions>()(
+export const useContentStore = create<State & Actions>()(
   immer((set) => ({
+    userData: {},
     language: "en",
     wordList: [],
     currPage: "no page",
@@ -24,5 +27,6 @@ export const useAuthStore = create<State & Actions>()(
     removeWord: (payload) =>
       set((state) => state.wordList.filter((item) => item !== payload)),
     setCurrPage: (page) => set((state) => (state.currPage = page)),
+    setUserData: (data) => set(() => ({ userData: data })),
   }))
 );
