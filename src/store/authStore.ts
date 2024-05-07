653cbfbdb8e9ee8394535f7e3ce3d32bd8ecf6f2
razzodev <razzo.des@gmail.com>
2024-05-07 18:09:52 +0530

    fix(store): signIn methods

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,38 +1,44 @@
 import { create } from "zustand";
-import { immer } from "zustand/middleware/immer";
+// import { immer } from "zustand/middleware/immer";
 
 type State = {
   user: any | null;
   uid: string | null;
   email: string | null;
   isAuthenticated: boolean;
-};
-
-type Actions = {
   setUser: (payload: any) => void;
-  setAuth: (authState: boolean) => void;
-  signIn: (payload: any) => void;
+  setAuth: (bool: boolean) => void;
+  signIn: (payload: any, callback?: VoidFunction) => void;
+  setEmail: (email: string) => void;
   signOut: (callback?: VoidFunction) => void;
 };
 
-export const useAuthStore = create<State & Actions>()(
-  immer((set) => ({
-    user: null,
-    uid: null,
-    email: null,
-    isAuthenticated: false,
-    setUser: (payload) => set((state) => (state.user = payload)),
-    setAuth: (bool) => set((state) => (state.isAuthenticated = bool)),
-    signIn: (payload) =>
-      set((state) => {
-        state.setAuth(true);
-        state.user = payload;
-      }),
-    signOut: (callback) =>
-      set((state) => {
-        state.setAuth(false);
-        state.user = null;
-        callback && callback();
-      }),
-  }))
-);
+export const useAuthStore = create<State>((set) => ({
+  user: null,
+  uid: null,
+  email: "foo@bar.com",
+  isAuthenticated: false,
+  setUser: (payload) => set(() => ({ user: payload })),
+  setAuth: (bool) => set(() => ({ isAuthenticated: bool })),
+  signIn: (payload, callback) => {
+    set(() => ({
+      user: payload,
+      uid: payload.uid,
+      isAuthenticated: true,
+      email: payload.email,
+    })),
+      callback && callback;
+  },
+  signOut: (callback) => {
+    set(() => ({
+      isAuthenticated: false,
+      email: null,
+      user: null,
+      uid: null,
+    })),
+      callback && callback();
+    console.log("signed in");
+  },
+  setEmail: (email) => set(() => ({ email: email })),
+}));
+
