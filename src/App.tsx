e115a66620af0f740dd48dd189cfe1552d814249
razzodev <razzo.des@gmail.com>
2024-05-07 17:40:13 +0530

    add(clickEvent): enter user data --initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
 import { useEffect } from "react";
-import { useAppStore } from "./store/appStore";
+import { useAuthStore } from "./store/authStore";
+import { useContentStore } from "./store/contentStore";
+import { auth, handleSigninClicked, isUserLoggedIn } from "./firebase/auth";
+import { readData, addData, readUserData } from "./firebase/firestore";
+import { user } from "./model";
 import "./App.css";
 
 function App() {
-  const bears = useAppStore((state) => state.bears);
-  const increase = useAppStore((state) => state.increasePopulation);
-  const removeAll = useAppStore((state) => state.removeAllBears);
-  const updateBears = useAppStore((state) => state.updateBears);
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    updateBears(parseInt(e.target.value));
+  const uid = useAuthStore((state) => state.uid);
+  const user = useAuthStore((state) => state.user);
+  const email = useAuthStore((state) => state.email);
+  const isAuth = useAuthStore((state) => state.isAuthenticated);
+  const setEmail = useAuthStore((state) => state.setEmail);
+  const setUserData = useContentStore((state) => state.setUserData);
   useEffect(() => {
-    increase();
+    isUserLoggedIn();
   }, []);
+  useEffect(() => {
+    uid && readUserData(uid).then((r) => setUserData(r));
+  }, [uid]);
 
+  const handleAddData = async () => {
+    let payload = {
+      displayName: user.displayName,
+      email: email,
+      uid: uid,
+      languages: [
+        {
+          name: "english",
+          categories: [
+            {
+              name: "general",
+              words: [],
+              icon: ":general-icon:",
+            },
+            {
+              name: "food & drinks",
+              words: [],
+              icon: ":fork-icon:",
+            },
+            {
+              name: "dating",
+              words: [],
+              icon: ":heart-icon:",
+            },
+          ],
+        },
+      ],
+    };
+    await addData("users", uid, payload);
+  };
   return (
     <>
-      <h1>Number of bears: {bears}</h1>
-      {/* <button onClick={increase}>add</button> */}
-      {/* <button onClick={removeAll}>reset</button> */}
-      {/* <input type="number" onChange={(e) => handleChange(e)} /> */}
+      <h1>User {email}</h1>
+      {isAuth && <h3>UID: {uid}</h3>}
+      <button onClick={handleSigninClicked}>Signin</button>
+      <button onClick={handleAddData}>add data</button>
     </>
   );
 }
