679d0203f5a7581f01e038e21b3c0ec96862dfe8
razzodev <razzo.des@gmail.com>
2024-05-07 18:09:22 +0530

    fix(state): export all stores

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,7 @@
-import * from './appStore'
+import * as authStore from "./authStore";
+import * as contentStore from "./contentStore";
+
+export const store = {
+  auth: authStore,
+  content: contentStore,
+};
