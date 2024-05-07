ccfabd501ee5f22aa30bd60a170da3e3ea1e839c
razzodev <razzo.des@gmail.com>
2024-05-07 19:13:52 +0530

    refactor: rename getAudio to getFile

--- src/firebase/storage.ts
+++ src/firebase/storage.ts
@@ -6,6 +6,6 @@ const storage = getStorage();
 // Create a storage reference from our storage service
 export const storageRef = ref(storage,'seinfeld.mp3');
 
-export function getAudio(path:string){
+export function getFile(path:string){
     return getDownloadURL(ref(storage,path))
 }
